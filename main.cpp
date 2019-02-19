29db547dacd5cba8381e6cb64cf877e57899f899
Kade Fortner <32686277+KadeFortner@users.noreply.github.com>
2019-02-19 10:40:14 -0500

    Project 2 Update
    to the main file

--- main.cpp
+++ main.cpp
@@ -9,26 +9,16 @@
 #include <iostream>
 #include "OS.hpp"
 #include "ram.hpp"
+#include "CPU.hpp"
 
 int main(int argc, const char * argv[]) {
     
-    /*
     cout << "PKstarstormOS" << endl;
-    OS myOS;
-    while(myOS.exitCode() == 0){
-        myOS.userCommand();
-    }
-    */
     
-    ram myRam;
-    cout << myRam.lookAt(0) << endl;
-    cout << myRam.lookAt(1) << endl;
-    cout << myRam.lookAt(2) << endl;
-    cout << myRam.lookAt(3) << endl;
-    cout << myRam.lookAt(4) << endl;
-    cout << myRam.lookAt(5) << endl;
-    myRam.assignRam("xo", 6);
-    cout << myRam.lookAt(6) << endl;
+    OS myOS;
+    RAM myRAM;
+    CPU myCPU;
+   
     
     return 0;
 }
