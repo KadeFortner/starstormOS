18ce119fba1f556e2e4d04ceb72097ae2b63628c
Kade Fortner <32686277+KadeFortner@users.noreply.github.com>
2019-03-19 15:12:06 -0400

    RAM.cpp 0.91 Update
    Shows taken memory spaces

--- ram.cpp
+++ ram.cpp
@@ -9,7 +9,7 @@
 #include "ram.hpp"
 
 RAM::RAM(){
-    cout << "The RAM has 30 spots available" << endl;
+    cout << "The RAM has been made and has 1000 spots available" << endl;
 }
 
 string RAM::lookAt(int location){
@@ -19,4 +19,34 @@ string RAM::lookAt(int location){
 void RAM::assignRAM(string item, int location){
     RAMmemory[location] = item;
     cout << "The item has been inserted into the RAM successfully" << endl;
+    int i = 0;
+    while(takenSlots[i] != 0){
+        i++;
+    }
+    takenSlots[i] = i;
+    cout << "Taken Slots has been updated" << endl;
+}
+
+void RAM::numOfFreeSlots(){
+    int i = 0;
+    while(freeSlots[i] != 0){
+        i++;
+    }
+    cout << "There are " << i << "number of free slots in RAM" << endl;
+}
+
+void RAM::numOfTakenSlots(){
+    int i = 0;
+    while(takenSlots[i] != 0){
+        i++;
+    }
+    cout << "There are " << i << " taken slots in RAM" << endl;
+}
+
+void RAM::showTakenSlots(){
+    int i = 0;
+    while(takenSlots[i] != 0){
+        cout << takenSlots[i] << endl;
+        i++;
+    }
 }
