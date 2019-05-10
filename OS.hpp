97dc9ec879c97ed0add3495fe059bf9d76455ddb
Kade Fortner <32686277+KadeFortner@users.noreply.github.com>
2019-05-10 02:28:46 -0400

    Update OS.hpp ver 0.99

--- OS.hpp
+++ OS.hpp
@@ -19,14 +19,14 @@
 using namespace std;
 
 
-class OS{
-    
-    static vector<string> history;
+class OS
+{
     
     private:
         double version;
         string user;
         int exit = 0;
+        vector<string> history;
     
     public:
     
@@ -41,30 +41,31 @@ class OS{
         //Setters
         void setVersion(double version){this->version = version;}
         void setUser(string user){this->user = user;}
+        void addToHistory(string command){history.push_back(command);}
     
         //Shows current date and time
-        //Works. Cannot set though. Want to have date and time as attributes
+        //Works. Cannot set though.
         void dateAndTime();
     
         //Shows Directory
         //Not currently programmed. Bug with reading files in the directory
         void showDirectory();
     
-        //Shows the history. Does not currently work. Having trouble inplementing a vector
+        //Shows the history. Does not currently work. 
         void showHistory();
     
         //Prints the user manual.
-        //Works
         void userManual();
     
         //Uses this for an exit condition.
-    
         int exitCode(){return exit;}
         void exitOS(){exit = 1;}
         void startOS();
     
         //Takes an input, and if able, executes corresponding command.
         void userCommand();
+    
+        void rename();
     
         //Prints list of commands
         void helpMenu();
