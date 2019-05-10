849f1aaf3ddfbb829848252aec7a4d16990d9ef5
Kade Fortner <32686277+KadeFortner@users.noreply.github.com>
2019-05-10 02:29:36 -0400

    Update RAM.cpp ver 0.99

--- ram.cpp
+++ ram.cpp
@@ -8,45 +8,40 @@
 
 #include "ram.hpp"
 
-RAM::RAM(){
+RAM::RAM()
+{
     cout << "The RAM has been made and has 1000 spots available" << endl;
 }
 
-string RAM::lookAt(int location){
+string RAM::lookAt(int location)
+{
     return RAMmemory[location];
 }
 
-void RAM::assignRAM(string item, int location){
-    RAMmemory[location] = item;
+void RAM::assignRAM(string item, int location)
+{
+    RAMmemory[location] = item; //The chosen item is added into the designated location in RAM
     cout << "The item has been inserted into the RAM successfully" << endl;
     int i = 0;
-    while(takenSlots[i] != 0){
+    while(takenMemory[i] != 0)
+    { //While the current spot in takenSlots is not free, go to the next spot until free
         i++;
     }
-    takenSlots[i] = i;
+    cout << "The current location is: " << location << endl;
+    cout <<"The current i value is: " << i << endl;
+    takenMemory[i+1] = location; //The item i will be inserted into Taken Slots @ position i
     cout << "Taken Slots has been updated" << endl;
 }
 
-void RAM::numOfFreeSlots(){
-    int i = 0;
-    while(freeSlots[i] != 0){
-        i++;
-    }
-    cout << "There are " << i << "number of free slots in RAM" << endl;
-}
 
-void RAM::numOfTakenSlots(){
+void RAM::numOfTakenSlots()
+{
     int i = 0;
-    while(takenSlots[i] != 0){
+    while(takenMemory[i] != 0)
+    {
         i++;
     }
     cout << "There are " << i << " taken slots in RAM" << endl;
 }
 
-void RAM::showTakenSlots(){
-    int i = 0;
-    while(takenSlots[i] != 0){
-        cout << takenSlots[i] << endl;
-        i++;
-    }
-}
+
