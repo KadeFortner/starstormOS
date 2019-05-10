983abddb824323c16575fde4d3da4238c9ba2b22
Kade Fortner <32686277+KadeFortner@users.noreply.github.com>
2019-05-10 02:28:13 -0400

    Update OS.cpp Ver 0.99
    Now has working history.

--- OS.cpp
+++ OS.cpp
@@ -12,45 +12,53 @@
 #include <locale>
 
 
-OS::OS(string user){
+OS::OS(string user)
+{
     this->user = user;
-    version = 0.91;
+    version = 0.99;
 }
 
-OS::OS(){
-    version = 0.91; //With Machine class, and 1000 Ram slots
+OS::OS()
+{
+    version = 0.99; //With Multi-Threading maybe. And working history
     user = "KadeTheMod";
     startOS();
-    userCommand();
 }
 
 //Prints current date and time.
-void OS::dateAndTime(){
+void OS::dateAndTime()
+{
     time_t t = time(NULL);
     tm* timePtr = localtime(&t);
     
     cout << "Date     " <<(timePtr->tm_mday)<<"/"<< (timePtr->tm_mon)+1 <<"/"<< (timePtr->tm_year)+1900<< endl;
     cout << "Time     " << (timePtr->tm_hour)<<":"<< (timePtr->tm_min)<<":"<< (timePtr->tm_sec) << endl;
-    //history.push_back("dateAndTime()");
-
+    addToHistory("dateAndTime()");
 }
 
-/*
-void OS::showHistory(){
+
+void OS::showHistory()
+{
     //Referenced
-    history.push_back("showHistory()");
-    cout << "\nVector elements are: ";
-    for(int i = 0; i <= history.size(); i++){
-        cout << history.front() << endl;
+    addToHistory("showHistory()");
+    cout << "\nHistory so far is: \n";
+    vector<string>::const_iterator i = history.begin();
+    while(i != history.end())
+    {
+        cout << *i << '\n';
+        i++;
     }
 }
-*/
 
-void OS::showDirectory(){
+
+void OS::showDirectory()
+{
     cout << "main.cpp\nos.cpp\nos.hpp\ndirectory.txt\nhistoryIn.txt\nhistoryOut.txt\nreferences.txt\n" << endl;
+    addToHistory("showDirectory()");
 }
 
-void OS::userManual(){
+void OS::userManual()
+{
     cout << "starstormOS User Manual" << endl;
     cout << "1. Table of Contents" << endl;
     cout << "2. Overview of the OS" << endl;
@@ -58,70 +66,89 @@ void OS::userManual(){
     cout << "4. Detailed Description of Each Command" << endl;
     cout << "5. Summary of Error Messages" << endl;
     cout << "6. Index" << endl;
-    //history.push_back("userManual()");
+    addToHistory("userManual()");
 }
 
 //Used for commands, Referenced.
-string toLowercase(string phrase){
-    std::string lowerPhrase = "";
-    std::locale loc;
-    std::string str=phrase;
+string toLowercase(string phrase)
+{
+    string lowerPhrase = "";
+    locale loc;
+    string str=phrase;
     //Only does one word for now. Must fix
-    for (std::string::size_type i=0; i<str.length(); ++i){
-        //std::cout << std::tolower(str[i],loc); For printing
-        lowerPhrase = lowerPhrase + std::tolower(str[i],loc);
+    for (string::size_type i=0; i<str.length(); ++i)
+    {
+        lowerPhrase = lowerPhrase + tolower(str[i],loc);
     }
     return lowerPhrase;
 }
 
-void OS::userCommand(){
+void OS::userCommand()
+{
     string command;
-    cout << "do something\n" << endl;
+    cout << "Do something!\n" << endl;
     cin >> command;
     //Changes the command to all lowercase so it can be compared
     command = toLowercase(command);
-    if(command == "version"){
+    if(command == "version")
+    {
         cout << "Version: " << getVersion() << "\n" << endl;
+        addToHistory("getVersion()");
     }
-    else if(command == "date"){
+    else if(command == "date")
+    {
         dateAndTime();
     }
-    else if(command == "directory"){
+    else if(command == "directory")
+    {
         cout << "Not made yet. oops\n" << endl;
     }
-    else if(command == "history"){
-        cout << "History\n" << endl;
+    else if(command == "history")
+    {
+        showHistory();
     }
-    else if(command == "rename"){
-        cout << "Nope.\n" << endl;
+    else if(command == "rename")
+    {
+        cout << "please help" << endl;
     }
-    else if(command == "exit"){
+    else if(command == "exit")
+    {
         cout << "bye\n" << endl;
         exitOS();
     }
-    else if(command == "help"){
+    else if(command == "help")
+    {
         helpMenu();
     }
-    else if(command == "manual"){
+    else if(command == "manual")
+    {
         userManual();
     }
-    else{
+    else
+    {
         cout << "do not understand\n" << endl;
     }
 }
 
-void OS::helpMenu(){
+void OS::helpMenu()
+{
     cout << "Here's some commands to try out\nShow the version: try 'version'\nShow the date: try 'date'\nShow the contents of the directory: try 'directory'\nShow the history of commands: try 'history'\nHave a batch file?: try typing in the filename followed by the extention. This will not work though.\nChange or add command names: try 'rename'\nExit: try 'exit'\n" << endl;
-    //history.push_back("helpMenu()");
+    addToHistory("helpMenu()");
 }
 
-void OS::endProgram(){
+void OS::endProgram()
+{
     exitOS();
     startOS();
     userManual();
 }
 
-void OS::startOS(){
+void OS::startOS()
+{
     exit = 0;
     cout << "Welcome to PKstarstormOS!" << endl;
+    while(exit == 0)
+    {
+        userCommand();
+    }
 }
